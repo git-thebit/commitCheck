0ae0aeb4051e7ec99b4155a7c1f3f9c4740b5007
gitthebit <piyusdeep001@hotmail.com>
2022-05-19 00:48:08 +0530

    unitTest.test.js and other Jest test

---
+++ force-app/main/default/lwc/unitTest/__tests__/sum.test.js
@@ -0,0 +1,10 @@
+import { sum } from "../sum";
+
+describe("sum()", () => {
+  it("should add 1 and 2 returning 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  it("Should not be negative", () => {
+    expect(sum(1, 2)).not.toBeLessThan(0);
+  });
+});
